bd4b9e33083d34ec636bab836d4d388f63088705
Stacey <stacey6105@gmail.com>
2023-02-12 23:38:04 +0000

    Creat pokemon-types bridging table

---
+++ db/development-data/pokemon-types.js
@@ -0,0 +1,1179 @@
+module.exports = [
+    {
+        name: "bulbasaur", 
+        type: "grass"
+    },
+    {
+        name: "bulbasaur", 
+        type: "poison"
+    },
+    {
+        name: "ivysaur", 
+        type: "grass"
+    },
+    {
+        name: "ivysaur", 
+        type: "poison"
+    },
+    {
+        name: "venusaur", 
+        type: "grass"
+    },
+    {
+        name: "venusaur", 
+        type: "poison"
+    },
+    {
+        name: "charmander", 
+        type: "fire"
+    },
+    {
+        name: "charmeleon", 
+        type: "fire"
+    },
+    {
+        name: "charizard", 
+        type: "fire"
+    },
+    {
+        name: "charizard", 
+        type: "flying"
+    },
+    {
+        name: "squirtle", 
+        type: "water"
+    },
+    {
+        name: "wartortle", 
+        type: "water"
+    },
+    {
+        name: "blastoise", 
+        type: "water"
+    },
+    {
+        name: "caterpie", 
+        type: "bug"
+    },
+    {
+        name: "metapod", 
+        type: "bug"
+    },
+    {
+        name: "butterfree", 
+        type: "bug"
+    },
+    {
+        name: "butterfree", 
+        type: "flying"
+    },
+    {
+        name: "weedle", 
+        type: "bug"
+    },
+    {
+        name: "weedle", 
+        type: "poison"
+    },
+    {
+        name: "kakuna", 
+        type: "bug"
+    },
+    {
+        name: "kakuna", 
+        type: "poison"
+    },
+    {
+        name: "beedrill", 
+        type: "bug"
+    },
+    {
+        name: "beedrill", 
+        type: "poison"
+    },
+    {
+        name: "pidgey", 
+        type: "normal"
+    },
+    {
+        name: "pidgey", 
+        type: "flying"
+    },
+    {
+        name: "pidgeotto", 
+        type: "normal"
+    },
+    {
+        name: "pidgeotto", 
+        type: "flying"
+    },
+    {
+        name: "pidgeot", 
+        type: "normal"
+    },
+    {
+        name: "pidgeot", 
+        type: "flying"
+    },
+    {
+        name: "ratata", 
+        type: "normal"
+    },
+    {
+        name: "raticate", 
+        type: "normal"
+    },
+    {
+        name: "spearow", 
+        type: "normal"
+    },
+    {
+        name: "spearow", 
+        type: "flying"
+    },
+    {
+        name: "fearow", 
+        type: "normal"
+    },
+    {
+        name: "fearow", 
+        type: "flying"
+    },
+    {
+        name: "ekans", 
+        type: "poison"
+    },
+    {
+        name: "arbok", 
+        type: "poison"
+    },
+    {
+        name: "pikachu", 
+        type: "electric"
+    },
+    {
+        name: "raichu", 
+        type: "electric"
+    },
+    {
+        name: "sandshrew", 
+        type: "ground"
+    },
+    {
+        name: "sandslash", 
+        type: "ground"
+    },
+    {
+        name: "nidoran-f", 
+        type: "poison"
+    },
+    {
+        name: "nidorina", 
+        type: "poison"
+    },
+    {
+        name: "nidoqueen", 
+        type: "poison"
+    },
+    {
+        name: "nidoqueen", 
+        type: "ground"
+    },
+    {
+        name: "nidoran-m", 
+        type: "poison"
+    },
+    {
+        name: "nidorino", 
+        type: "poison"
+    },
+    {
+        name: "nidoking", 
+        type: "poison"
+    },
+    {
+        name: "nidoking", 
+        type: "ground"
+    },
+    {
+        name: "clefairy", 
+        type: "fairy"
+    },
+    {
+        name: "clefable", 
+        type: "fairy"
+    },
+    {
+        name: "vulpix", 
+        type: "fire"
+    },
+    {
+        name: "ninetails", 
+        type: "fire"
+    },
+    {
+        name: "jigglypuff", 
+        type: "normal"
+    },
+    {
+        name: "jigglypuff", 
+        type: "fairy"
+    },
+    {
+        name: "wigglytuff", 
+        type: "normal"
+    },
+    {
+        name: "wigglytuff", 
+        type: "fairy"
+    },
+    {
+        name: "zubat", 
+        type: "poison"
+    },
+    {
+        name: "zubat", 
+        type: "flying"
+    },
+    {
+        name: "golbat", 
+        type: "poison"
+    },
+    {
+        name: "golbat", 
+        type: "flying"
+    },
+    {
+        name: "oddish", 
+        type: "grass"
+    },
+    {
+        name: "oddish", 
+        type: "poison"
+    },
+    {
+        name: "gloom", 
+        type: "grass"
+    },
+    {
+        name: "gloom", 
+        type: "poison"
+    },
+    {
+        name: "vileplume", 
+        type: "grass"
+    },
+    {
+        name: "vileplume", 
+        type: "poison"
+    },
+    {
+        name: "paras", 
+        type: "bug"
+    },
+    {
+        name: "paras", 
+        type: "grass"
+    },
+    {
+        name: "parasect", 
+        type: "bug"
+    },
+    {
+        name: "parasect", 
+        type: "grass"
+    },
+    {
+        name: "venonat", 
+        type: "bug"
+    },
+    {
+        name: "venonat", 
+        type: "poison"
+    },
+    {
+        name: "venomoth", 
+        type: "bug"
+    },
+    {
+        name: "venomoth", 
+        type: "poison"
+    },
+    {
+        name: "diglet", 
+        type: "ground"
+    },
+    {
+        name: "dugtrio", 
+        type: "ground"
+    },
+    {
+        name: "meowth", 
+        type: "normal"
+    },
+    {
+        name: "persian", 
+        type: "normal"
+    },
+    {
+        name: "psyduck", 
+        type: "water"
+    },
+    {
+        name: "golduck", 
+        type: "water"
+    },
+    {
+        name: "mankey", 
+        type: "fighting"
+    },
+    {
+        name: "primeape", 
+        type: "fighting"
+    },
+    {
+        name: "growlithe", 
+        type: "fire"
+    },
+    {
+        name: "arcanine", 
+        type: "fire"
+    },
+    {
+        name: "poliwag", 
+        type: "water"
+    },
+    {
+        name: "poliwhirl", 
+        type: "water"
+    },
+    {
+        name: "poliwrath", 
+        type: "water"
+    },
+    {
+        name: "poliwrath", 
+        type: "fighting"
+    },
+    {
+        name: "abra", 
+        type: "psychic"
+    },
+    {
+        name: "kadabra", 
+        type: "psychic"
+    },
+    {
+        name: "alakazam", 
+        type: "psychic"
+    },
+    {
+        name: "machop", 
+        type: "fighting"
+    },
+    {
+        name: "machoke", 
+        type: "fighting"
+    },
+    {
+        name: "machamp", 
+        type: "fighting"
+    },
+    {
+        name: "bellsprout", 
+        type: "grass"
+    },
+    {
+        name: "bellsprout", 
+        type: "poison"
+    },
+    {
+        name: "weepinbell", 
+        type: "grass"
+    },
+    {
+        name: "weepinbell", 
+        type: "poison"
+    },
+    {
+        name: "victreebel", 
+        type: "grass"
+    },
+    {
+        name: "victreebel", 
+        type: "poison"
+    },
+    {
+        name: "tentacool", 
+        type: "water"
+    },
+    {
+        name: "tentacool", 
+        type: "poison"
+    },
+    {
+        name: "tentacruel", 
+        type: "water"
+    },
+    {
+        name: "tentacruel", 
+        type: "poison"
+    },
+    {
+        name: "geodude", 
+        type: "rock"
+    },
+    {
+        name: "geodude", 
+        type: "ground"
+    },
+    {
+        name: "graveler", 
+        type: "rock"
+    },
+    {
+        name: "graveler", 
+        type: "ground"
+    },
+    {
+        name: "golem", 
+        type: "rock"
+    },
+    {
+        name: "golem", 
+        type: "ground"
+    },
+    {
+        name: "ponyta", 
+        type: "fire"
+    },
+    {
+        name: "rapidash", 
+        type: "fire"
+    },
+    {
+        name: "slowpoke", 
+        type: "water"
+    },
+    {
+        name: "slowpoke", 
+        type: "psychic"
+    },
+    {
+        name: "slowbro", 
+        type: "water"
+    },
+    {
+        name: "slowbro", 
+        type: "psychic"
+    },
+    {
+        name: "magnemite", 
+        type: "electric"
+    },
+    {
+        name: "magnemite", 
+        type: "steel"
+    },
+    {
+        name: "magneton", 
+        type: "electric"
+    },
+    {
+        name: "magneton", 
+        type: "steel"
+    },
+    {
+        name: "farfetch'd", 
+        type: "normal"
+    },
+    {
+        name: "farfetch'd", 
+        type: "flying"
+    },
+    {
+        name: "doduo", 
+        type: "normal"
+    },
+    {
+        name: "doduo", 
+        type: "flying"
+    },
+    {
+        name: "dodrio", 
+        type: "normal"
+    },
+    {
+        name: "dodrio", 
+        type: "flying"
+    },
+    {
+        name: "seel", 
+        type: "water"
+    },
+    {
+        name: "dewgong", 
+        type: "water"
+    },
+    {
+        name: "dewgong", 
+        type: "ice"
+    },
+    {
+        name: "grimer", 
+        type: ""
+    },
+    {
+        name: "muk", 
+        type: ""
+    },
+    {
+        name: "shellder", 
+        type: ""
+    },
+    {
+        name: "cloyster", 
+        type: ""
+    },
+    {
+        name: "gastly", 
+        type: ""
+    },
+    {
+        name: "haunter", 
+        type: ""
+    },
+    {
+        name: "gengar", 
+        type: ""
+    },
+    {
+        name: "onix", 
+        type: ""
+    },
+    {
+        name: "drowzee", 
+        type: ""
+    },
+    {
+        name: "hypno", 
+        type: ""
+    },
+    {
+        name: "krabby", 
+        type: ""
+    },
+    {
+        name: "kingler", 
+        type: ""
+    },
+    {
+        name: "voltorb", 
+        type: ""
+    },
+    {
+        name: "electrode", 
+        type: ""
+    },
+    {
+        name: "exeggcute", 
+        type: ""
+    },
+    {
+        name: "exeggutor", 
+        type: ""
+    },
+    {
+        name: "cubone", 
+        type: ""
+    },
+    {
+        name: "marowak", 
+        type: ""
+    },
+    {
+        name: "hitmonlee", 
+        type: ""
+    },
+    {
+        name: "hitmonchan", 
+        type: ""
+    },
+    {
+        name: "lickitung", 
+        type: ""
+    },
+    {
+        name: "koffing", 
+        type: ""
+    },
+    {
+        name: "weezing", 
+        type: ""
+    },
+    {
+        name: "rhyhorn", 
+        type: ""
+    },
+    {
+        name: "rhydon", 
+        type: ""
+    },
+    {
+        name: "chansey", 
+        type: ""
+    },
+    {
+        name: "tangela", 
+        type: ""
+    },
+    {
+        name: "kangaskhan", 
+        type: ""
+    },
+    {
+        name: "horsea", 
+        type: ""
+    },
+    {
+        name: "seadra", 
+        type: ""
+    },
+    {
+        name: "goldeen", 
+        type: ""
+    },
+    {
+        name: "seaking", 
+        type: ""
+    },
+    {
+        name: "staryu", 
+        type: ""
+    },
+    {
+        name: "starmie", 
+        type: ""
+    },
+    {
+        name: "mr. mime", 
+        type: ""
+    },
+    {
+        name: "scyther", 
+        type: ""
+    },
+    {
+        name: "jynx", 
+        type: ""
+    },
+    {
+        name: "electabuzz", 
+        type: ""
+    },
+    {
+        name: "magmar", 
+        type: ""
+    },
+    {
+        name: "pinsir", 
+        type: ""
+    },
+    {
+        name: "tauros", 
+        type: ""
+    },
+    {
+        name: "magikarp", 
+        type: ""
+    },
+    {
+        name: "gyarados", 
+        type: ""
+    },
+    {
+        name: "lapras", 
+        type: ""
+    },
+    {
+        name: "ditto", 
+        type: ""
+    },
+    {
+        name: "eevee", 
+        type: ""
+    },
+    {
+        name: "vaporeon", 
+        type: ""
+    },
+    {
+        name: "jolteon", 
+        type: ""
+    },
+    {
+        name: "flareon", 
+        type: ""
+    },
+    {
+        name: "porygon", 
+        type: ""
+    },
+    {
+        name: "omanyte", 
+        type: ""
+    },
+    {
+        name: "omastar", 
+        type: ""
+    },
+    {
+        name: "kabuto", 
+        type: ""
+    },
+    {
+        name: "kabutops", 
+        type: ""
+    },
+    {
+        name: "aerodactyl", 
+        type: ""
+    },
+    {
+        name: "snorlax", 
+        type: ""
+    },
+    {
+        name: "articuno", 
+        type: ""
+    },
+    {
+        name: "zapdos", 
+        type: ""
+    },
+    {
+        name: "moltres", 
+        type: ""
+    },
+    {
+        name: "dratini", 
+        type: ""
+    },
+    {
+        name: "dragonair", 
+        type: ""
+    },
+    {
+        name: "dragonite", 
+        type: ""
+    },
+    {
+        name: "mewtwo", 
+        type: ""
+    },
+    {
+        name: "mew", 
+        type: ""
+    },
+    //generation 2
+    {
+        name: "chikorita", 
+        type: ""
+    },
+    {
+        name: "bayleaf", 
+        type: ""
+    },
+    {
+        name: "meganium", 
+        type: ""
+    },
+    {
+        name: "cyndaquil", 
+        type: ""
+    },
+    {
+        name: "quilava", 
+        type: ""
+    },
+    {
+        name: "typhlosion", 
+        type: ""
+    },
+    {
+        name: "totodile", 
+        type: ""
+    },
+    {
+        name: "croconaw", 
+        type: ""
+    },
+    {
+        name: "feraligator", 
+        type: ""
+    },
+    {
+        name: "sentret", 
+        type: ""
+    },
+    {
+        name: "furret", 
+        type: ""
+    },
+    {
+        name: "hoothoot", 
+        type: ""
+    },
+    {
+        name: "noctowl", 
+        type: ""
+    },
+    {
+        name: "ledyba", 
+        type: ""
+    },
+    {
+        name: "ledian", 
+        type: ""
+    },
+    {
+        name: "spinarak", 
+        type: ""
+    },
+    {
+        name: "ariados", 
+        type: ""
+    },
+    {
+        name: "crobat", 
+        type: ""
+    },
+    {
+        name: "chinchou", 
+        type: ""
+    },
+    {
+        name: "lantern", 
+        type: ""
+    },
+    {
+        name: "pichu", 
+        type: ""
+    },
+    {
+        name: "cleffa", 
+        type: ""
+    },
+    {
+        name: "igglybuff", 
+        type: ""
+    },
+    {
+        name: "togepi", 
+        type: ""
+    },
+    {
+        name: "togetic", 
+        type: ""
+    },
+    {
+        name: "natu", 
+        type: ""
+    },
+    {
+        name: "xatu", 
+        type: ""
+    },
+    {
+        name: "mareep", 
+        type: ""
+    },
+    {
+        name: "flaaffy", 
+        type: ""
+    },
+    {
+        name: "ampharos", 
+        type: ""
+    },
+    {
+        name: "bellossom", 
+        type: ""
+    },
+    {
+        name: "marill", 
+        type: ""
+    },
+    {
+        name: "azumarill", 
+        type: ""
+    },
+    {
+        name: "sudowoodo", 
+        type: ""
+    },
+    {
+        name: "politoed", 
+        type: ""
+    },
+    {
+        name: "hoppip", 
+        type: ""
+    },
+    {
+        name: "skiploom", 
+        type: ""
+    },
+    {
+        name: "jumpluff", 
+        type: ""
+    },
+    {
+        name: "aipom", 
+        type: ""
+    },
+    {
+        name: "sunkern", 
+        type: ""
+    },
+    {
+        name: "sunflora", 
+        type: ""
+    },
+    {
+        name: "yanma", 
+        type: ""
+    },
+    {
+        name: "wooper", 
+        type: ""
+    },
+    {
+        name: "gloom", 
+        type: ""
+    },
+    {
+        name: "espeon", 
+        type: ""
+    },
+    {
+        name: "umbreon", 
+        type: ""
+    },
+    {
+        name: "murkrow", 
+        type: ""
+    },
+    {
+        name: "slowking", 
+        type: ""
+    },
+    {
+        name: "misdreavus", 
+        type: ""
+    },
+    {
+        name: "unown", 
+        type: ""
+    },
+    {
+        name: "wobbuffet", 
+        type: ""
+    },
+    {
+        name: "girafarig", 
+        type: ""
+    },
+    {
+        name: "pineco", 
+        type: ""
+    },
+    {
+        name: "forretress", 
+        type: ""
+    },
+    {
+        name: "dunsparce", 
+        type: ""
+    },
+    {
+        name: "gligar", 
+        type: ""
+    },
+    {
+        name: "steelix", 
+        type: ""
+    },
+    {
+        name: "snubbull", 
+        type: ""
+    },
+    {
+        name: "granbull", 
+        type: ""
+    },
+    {
+        name: "qwilfish", 
+        type: ""
+    },
+    {
+        name: "scizor", 
+        type: ""
+    },
+    {
+        name: "shuckle", 
+        type: ""
+    },
+    {
+        name: "heracross", 
+        type: ""
+    },
+    {
+        name: "sneasel", 
+        type: ""
+    },
+    {
+        name: "teddiursa", 
+        type: ""
+    },
+    {
+        name: "ursaring", 
+        type: ""
+    },
+    {
+        name: "slugma", 
+        type: ""
+    },
+    {
+        name: "magcargo", 
+        type: ""
+    },
+    {
+        name: "swinub", 
+        type: ""
+    },
+    {
+        name: "piloswine", 
+        type: ""
+    },
+    {
+        name: "corsola", 
+        type: ""
+    },
+    {
+        name: "remoraid", 
+        type: ""
+    },
+    {
+        name: "octillery", 
+        type: ""
+    },
+    {
+        name: "delibird", 
+        type: ""
+    },
+    {
+        name: "mantine", 
+        type: ""
+    },
+    {
+        name: "skarmory", 
+        type: ""
+    },
+    {
+        name: "houndour", 
+        type: ""
+    },
+    {
+        name: "houndoom", 
+        type: ""
+    },
+    {
+        name: "kingdra", 
+        type: ""
+    },
+    {
+        name: "phanpy", 
+        type: ""
+    },
+    {
+        name: "donphan", 
+        type: ""
+    },
+    {
+        name: "porygon 2", 
+        type: ""
+    },
+    {
+        name: "stantler", 
+        type: ""
+    },
+    {
+        name: "smeargle", 
+        type: ""
+    },
+    {
+        name: "tyrogue", 
+        type: ""
+    },
+    {
+        name: "hitmontop", 
+        type: ""
+    },
+    {
+        name: "smoochum", 
+        type: ""
+    },
+    {
+        name: "elekid", 
+        type: ""
+    },
+    {
+        name: "magby", 
+        type: ""
+    },
+    {
+        name: "miltankr", 
+        type: ""
+    },
+    {
+        name: "blissey", 
+        type: ""
+    },
+    {
+        name: "raikou", 
+        type: ""
+    },
+    {
+        name: "entei", 
+        type: ""
+    },
+    {
+        name: "suicune", 
+        type: ""
+    },
+    {
+        name: "larvitar", 
+        type: ""
+    },
+    {
+        name: "pupitar", 
+        type: ""
+    },
+    {
+        name: "tyranitar", 
+        type: ""
+    },
+    {
+        name: "lugia", 
+        type: ""
+    },
+    {
+        name: "ho-oh", 
+        type: ""
+    },
+    {
+        name: "celebi", 
+        type: ""
+    }
+]
