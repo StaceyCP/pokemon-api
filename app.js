737903ad77b8929b40c9e2330fe93c276fbba35d
Stacey <stacey6105@gmail.com>
2023-02-20 20:19:45 +0000

    Initial set up of express server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { handleServerErrors } = require("./errors");
+const app = express();
+const apiRouter = require("./routes/apiRouter");
+
+app.use("/api", apiRouter);
+
+app.use(handleServerErrors);
+
+module.exports = app;
