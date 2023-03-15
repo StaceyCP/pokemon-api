19c70691170a46c625d68b74a7f5c485f6b4d643
Stacey <stacey6105@gmail.com>
2023-03-15 13:59:58 +0000

    Enabled CORS

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { handleServerErrors, handleCustomErrors } = require("./errors");
 const app = express();
 const apiRouter = require("./routes/apiRouter");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
